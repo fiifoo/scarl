00f1407821eca433d97dfd2ad619a1e8095e7004
Taavi Lappalainen <taavi.lappalainen@gmail.com>
2017-01-22 21:58:37 +0200

    Line of sight

---
+++ scarl/src/main/scala/io/github/fiifoo/scarl/geometry/Los.scala
@@ -0,0 +1,15 @@
+package io.github.fiifoo.scarl.geometry
+
+import io.github.fiifoo.scarl.core.{Location, State}
+
+object Los {
+
+  def apply(s: State)(line: Vector[Location]): Boolean = {
+    val _blocked = blocked(s) _
+    !line.tail.dropRight(1).exists(_blocked)
+  }
+
+  private def blocked(s: State)(location: Location): Boolean = {
+    s.index.entities.location.isDefinedAt(location)
+  }
+}
