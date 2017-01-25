cfc55cb414659c61306e81bc2b0c4c883afcbd1a
Taavi Lappalainen <taavi.lappalainen@gmail.com>
2017-01-25 23:45:17 +0200

    First AI implementation which attacks closest enemy

--- scarl/src/main/scala/io/github/fiifoo/scarl/ai/ActionDecider.scala
+++ scarl/src/main/scala/io/github/fiifoo/scarl/ai/ActionDecider.scala
@@ -1,11 +1,29 @@
 package io.github.fiifoo.scarl.ai
 
-import io.github.fiifoo.scarl.action.PassAction
+import io.github.fiifoo.scarl.action.{AttackAction, MoveAction, PassAction}
 import io.github.fiifoo.scarl.core.State
 import io.github.fiifoo.scarl.core.action.{Action, ActionDecider => ActionDeciderTrait}
 import io.github.fiifoo.scarl.core.entity.Creature
+import io.github.fiifoo.scarl.geometry.Line
 
 object ActionDecider extends ActionDeciderTrait {
 
-  def apply(s: State, actor: Creature): Action = PassAction()
+  def apply(s: State, actor: Creature): Action = {
+    val enemy = SeekEnemy(s, actor)
+
+    if (enemy.isDefined) {
+      attack(actor, enemy.get)
+    } else {
+      PassAction()
+    }
+  }
+
+  private def attack(actor: Creature, enemy: Creature): Action = {
+    val line = Line(actor.location, enemy.location)
+    if (line.size == 2) {
+      AttackAction(enemy)
+    } else {
+      MoveAction(line(1))
+    }
+  }
 }
