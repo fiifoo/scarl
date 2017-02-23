72eb8289fcc85912b8958a9ed05d42421aeb286e
Taavi Lappalainen <taavi.lappalainen@gmail.com>
2017-02-23 21:32:45 +0200

    Weighted random selection from list of choices

--- scarl/src/main/scala/io/github/fiifoo/scarl/core/Rng.scala
+++ scarl/src/main/scala/io/github/fiifoo/scarl/core/Rng.scala
@@ -1,6 +1,39 @@
 package io.github.fiifoo.scarl.core
 
+import io.github.fiifoo.scarl.core.Rng.WeightedChoices
+
 import scala.util.Random
+
+object Rng {
+
+  case class WeightedChoices[T](choices: Iterable[(T, Int)]) {
+    type BuildResult = (List[(T, Int)], Int)
+
+    val (cumulative, total) = build()
+
+    def apply(random: Random): T = {
+      val value = random.nextInt(total) + 1
+
+      val result = cumulative find (choice => {
+        choice._2 >= value
+      })
+
+      result.get._1
+    }
+
+    private def build(): BuildResult = {
+      val (cumulative, total) = choices.foldLeft[BuildResult]((List(), 0))((result, choice) => {
+        val total = result._2 + choice._2
+        val cumulative = (choice._1, total) :: result._1
+
+        (cumulative, total)
+      })
+
+      (cumulative.reverse, total)
+    }
+  }
+
+}
 
 case class Rng(seed: Int) {
 
@@ -16,4 +49,9 @@ case class Rng(seed: Int) {
     (random.nextInt(n), Rng(random.nextInt()))
   }
 
+  def nextChoice[T](choices: WeightedChoices[T]): (T, Rng) = {
+    val random = new Random(seed)
+
+    (choices(random), Rng(random.nextInt()))
+  }
 }
