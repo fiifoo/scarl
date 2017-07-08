5bc4ae50af1ab7b1ecb157f9c07055b70c850f94
Taavi Lappalainen <taavi.lappalainen@gmail.com>
2017-07-08 11:22:52 +0300

    Use draw helper for events & highlight

--- scarl-ui/src/screen/layers/event.js
+++ scarl-ui/src/screen/layers/event.js
@@ -1,11 +1,12 @@
-import { EVENT_DURATION, EXPLOSION_COLOR, HIGHLIGHT_COLOR, SHOT_COLOR, TILE_MIDDLE, TILE_SIZE } from '../const'
-import { clearContext, createCanvas } from '../utils'
+import { EVENT_DURATION, EXPLOSION_COLOR, HIGHLIGHT_COLOR, SHOT_COLOR } from '../const'
+import { clearContext, createCanvas, createDraw } from '../utils'
 
 const filterEvents = e => e.type !== 'GenericEvent'
 
 export default () => {
     const canvas = createCanvas()
     const context = canvas.getContext('2d')
+    const draw = createDraw(context)
 
     const clear = () => clearContext(context)
 
@@ -37,53 +38,20 @@ export default () => {
     }
 
     const renderExplosion = ({location}) => {
-        const x = location.x * TILE_SIZE
-        const y = location.y * TILE_SIZE
-
-        context.fillStyle = EXPLOSION_COLOR
-        context.fillRect(x, y, TILE_SIZE, TILE_SIZE)
+        draw.fill(EXPLOSION_COLOR)(location)
     }
 
     const renderHit = ({location}) => {
-        const x = location.x * TILE_SIZE
-        const y = location.y * TILE_SIZE
-
-        context.fillStyle = HIGHLIGHT_COLOR
-        context.fillRect(x, y, TILE_SIZE, TILE_SIZE)
+        draw.fill(HIGHLIGHT_COLOR)(location)
     }
 
     const renderMoveMissile = ({from, to}) => {
-        const from_x = from.x * TILE_SIZE + TILE_MIDDLE + 0.5
-        const from_y = from.y * TILE_SIZE + TILE_MIDDLE + 0.5
-        const to_x = to.x * TILE_SIZE + TILE_MIDDLE + 0.5
-        const to_y = to.y * TILE_SIZE + TILE_MIDDLE + 0.5
-
-        context.strokeStyle = SHOT_COLOR
-
-        context.beginPath()
-        context.moveTo(from_x, from_y)
-        context.lineTo(to_x, to_y)
-        context.stroke()
+        draw.line(SHOT_COLOR)(from)(to)
     }
 
     const renderShot = ({from, to}) => {
-        const from_x = from.x * TILE_SIZE + TILE_MIDDLE + 0.5
-        const from_y = from.y * TILE_SIZE + TILE_MIDDLE + 0.5
-        const to_x = to.x * TILE_SIZE + TILE_MIDDLE + 0.5
-        const to_y = to.y * TILE_SIZE + TILE_MIDDLE + 0.5
-
-        context.fillStyle = SHOT_COLOR
-        context.strokeStyle = SHOT_COLOR
-
-        context.beginPath()
-        context.moveTo(from_x, from_y)
-        context.lineTo(to_x, to_y)
-        context.stroke()
-
-        context.beginPath()
-        context.moveTo(to_x, to_y)
-        context.arc(to_x, to_y, 3, 0, Math.PI * 2)
-        context.fill()
+        draw.line(SHOT_COLOR)(from)(to)
+        draw.dot(SHOT_COLOR)(to)
     }
 
     return {
--- scarl-ui/src/screen/layers/highlight.js
+++ scarl-ui/src/screen/layers/highlight.js
@@ -1,10 +1,9 @@
-import { HIGHLIGHT_COLOR, TILE_SIZE } from '../const'
-import { clearContext, createCanvas } from '../utils'
+import { HIGHLIGHT_COLOR } from '../const'
+import { clearContext, createCanvas, createDraw } from '../utils'
 
 export default () => {
     const canvas = createCanvas()
     const context = canvas.getContext('2d')
-    context.fillStyle = HIGHLIGHT_COLOR
 
     const clear = () => clearContext(context)
 
@@ -14,12 +13,7 @@ export default () => {
         locations.forEach(render)
     }
 
-    const render = location => {
-        const x = location.x * TILE_SIZE
-        const y = location.y * TILE_SIZE
-
-        context.fillRect(x, y, TILE_SIZE, TILE_SIZE)
-    }
+    const render = createDraw(context).fill(HIGHLIGHT_COLOR)
 
     return {
         canvas,
