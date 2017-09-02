b791c60b0c62fbd84573bc5d3c35a1fecf848640
Taavi Lappalainen <taavi.lappalainen@gmail.com>
2017-09-02 10:33:35 +0300

    Updated ui-project

--- scarl-ui/webpack.config.js
+++ scarl-ui/webpack.config.js
@@ -1,26 +1,27 @@
-var ExtractTextPlugin = require('extract-text-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
     entry: './src/index.jsx',
     output: {
         filename: 'index.js',
-        path: './dist'
+        path: __dirname + '/dist',
     },
     module: {
-        loaders: [
+        rules: [
             { test: /\.js|jsx$/, exclude: /node_modules/, loader: 'babel-loader' },
-            { test: /\.css$/, loader: ExtractTextPlugin.extract('style', 'css-loader') },
-            { test: /\.png$/, loader: 'url-loader?limit=100000' },
-            { test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
-            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
-            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
-            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' }
+            { test: /\.css$/, use: ExtractTextPlugin.extract({fallback: 'style-loader', use: 'css-loader'})},
+            { test: /\.(png|jpg|gif)$/, use: [{loader: 'url-loader', options: {limit: 8192}}]},
+            { test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, use: [{loader: 'url-loader', options: {limit: 8192,mimetype: 'application/font-woff'}}]},
+            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, use: [{loader: 'url-loader', options: {limit: 8192, mimetype: 'application/octet-stream'}}]},
+            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader'},
+            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, use: [{loader: 'url-loader', options: {limit: 8192, mimetype: 'image/svg+xml'}}]},
         ]
     },
     plugins: [
         new ExtractTextPlugin('index.css'),
     ],
     devServer: {
-        port: 80
+        inline: false,
+        port: 80,
     }
 }
