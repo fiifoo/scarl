a691b0567adefc32dc9c9db7030d92826c2fc0ee
Taavi Lappalainen <taavi.lappalainen@gmail.com>
2017-11-29 00:41:02 +0200

    Move explosion calculation to rule class & adjust

--- scarl/src/main/scala/io/github/fiifoo/scarl/effect/combat/ExplosionEffect.scala
+++ scarl/src/main/scala/io/github/fiifoo/scarl/effect/combat/ExplosionEffect.scala
@@ -2,9 +2,9 @@ package io.github.fiifoo.scarl.effect.combat
 
 import io.github.fiifoo.scarl.core.creature.Stats.Explosive
 import io.github.fiifoo.scarl.core.effect.{Effect, EffectResult}
-import io.github.fiifoo.scarl.core.entity.{EntityId, ItemId, LocatableId, WallId}
+import io.github.fiifoo.scarl.core.entity.LocatableId
 import io.github.fiifoo.scarl.core.{Location, State}
-import io.github.fiifoo.scarl.geometry.{Line, Obstacle, Shape}
+import io.github.fiifoo.scarl.rule.ExplosionRule
 
 case class ExplosionEffect(source: LocatableId,
                            location: Location,
@@ -13,39 +13,11 @@ case class ExplosionEffect(source: LocatableId,
                           ) extends Effect {
 
   def apply(s: State): EffectResult = {
-    val obstacle = Obstacle.explosion(s) _
-    val step = stats.blast / stats.radius
-    val locations = Shape.circle(location, stats.radius) filterNot blocked(s, step, obstacle)
+    val locations = ExplosionRule(s, location, stats)
 
     EffectResult(
       (locations map (ExplosionLocationEffect(source, _, stats, Some(this)))).toList
     )
   }
 
-  private def blocked(s: State, step: Int, obstacle: Location => Option[EntityId])(target: Location): Boolean = {
-    val (result, _) = (Line(location, target) foldLeft(false, stats.blast)) ((carry, location) => {
-      val (result, blast) = carry
-
-      if (result) {
-        carry
-      } else {
-        (obstacle(location) collect {
-          case item: ItemId => calculateBlast(item(s).door flatMap (_.hardness), blast, step)
-          case wall: WallId => calculateBlast(wall(s).hardness, blast, step)
-        }) getOrElse(false, Math.max(blast - step, 0))
-      }
-    })
-
-    result
-  }
-
-  private def calculateBlast(hardness: Option[Int], blast: Int, step: Int): (Boolean, Int) = {
-    hardness map (hardness => {
-      if (hardness > blast) {
-        (true, 0)
-      } else {
-        (false, Math.max(blast - hardness - step, 0))
-      }
-    }) getOrElse(true, 0)
-  }
 }
---
+++ scarl/src/main/scala/io/github/fiifoo/scarl/rule/ExplosionRule.scala
@@ -0,0 +1,48 @@
+package io.github.fiifoo.scarl.rule
+
+import io.github.fiifoo.scarl.core.creature.Stats.Explosive
+import io.github.fiifoo.scarl.core.entity.{EntityId, ItemId, WallId}
+import io.github.fiifoo.scarl.core.{Location, State}
+import io.github.fiifoo.scarl.geometry.{Line, Obstacle, Shape}
+
+object ExplosionRule {
+
+  def apply(s: State, location: Location, stats: Explosive): Set[Location] = {
+    val step = stats.blast / stats.radius / 2
+    val obstacle = Obstacle.explosion(s) _
+
+    Shape.circle(location, stats.radius) filterNot blocked(s, step, location, stats, obstacle)
+  }
+
+  private def blocked(s: State,
+                      step: Int,
+                      source: Location,
+                      stats: Explosive,
+                      obstacle: Location => Option[EntityId]
+                     )(target: Location): Boolean = {
+    val (result, _) = (Line(source, target).tail foldLeft(false, stats.blast)) ((carry, location) => {
+      val (result, blast) = carry
+
+      if (result) {
+        carry
+      } else {
+        (obstacle(location) collect {
+          case item: ItemId => calculateBlast(item(s).door flatMap (_.hardness), blast, step)
+          case wall: WallId => calculateBlast(wall(s).hardness, blast, step)
+        }) getOrElse(false, Math.max(blast - step, 0))
+      }
+    })
+
+    result
+  }
+
+  private def calculateBlast(hardness: Option[Int], blast: Int, step: Int): (Boolean, Int) = {
+    hardness map (hardness => {
+      if (hardness > blast) {
+        (true, 0)
+      } else {
+        (false, Math.max(blast - hardness - step, 0))
+      }
+    }) getOrElse(true, 0)
+  }
+}
