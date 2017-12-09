5f5adb438457aa980056149e732e64b084c7a506
Taavi Lappalainen <taavi.lappalainen@gmail.com>
2017-12-09 21:03:07 +0200

    Improve admin api response handling

--- scarl-admin/src/api/index.js
+++ scarl-admin/src/api/index.js
@@ -2,19 +2,28 @@ const headers = {
     'Accept': 'application/json',
     'Content-Type': 'application/json'
 }
+
+const NO_CONTENT = 204
+
+const processResponse = errorMessage => response => {
+    if (response.ok) {
+        if (response.status === NO_CONTENT) {
+            return null
+        } else {
+            return response.json()
+        }
+    } else {
+        throw Error(errorMessage)
+    }
+}
+
 export const fetchSummary = () => window.fetch('admin/summary', {
     headers,
     method: 'GET',
-}).then(response => {
-    if (response.ok) {
-        return response.json()
-    } else {
-        throw Error('Fetching summary failed')
-    }
-})
+}).then(processResponse('Fetching summary failed'))
 
 export const save = data => window.fetch('admin', {
     headers,
     method: 'PUT',
     body: JSON.stringify(data),
-})
+}).then(processResponse('Saving data failed'))
