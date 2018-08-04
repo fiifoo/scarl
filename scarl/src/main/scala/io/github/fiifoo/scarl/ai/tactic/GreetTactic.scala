1ec0da73490f1f77023f032e523f7eb40be2a4a7
Taavi Lappalainen <taavi.lappalainen@gmail.com>
2018-08-04 20:47:05 +0300

    Fix greet tactic

--- scarl/src/main/scala/io/github/fiifoo/scarl/ai/tactic/GreetTactic.scala
+++ scarl/src/main/scala/io/github/fiifoo/scarl/ai/tactic/GreetTactic.scala
@@ -3,20 +3,22 @@ package io.github.fiifoo.scarl.ai.tactic
 import io.github.fiifoo.scarl.action.PassAction
 import io.github.fiifoo.scarl.ai.intention.CheckGreetIntention
 import io.github.fiifoo.scarl.core.State
+import io.github.fiifoo.scarl.core.action.Action
 import io.github.fiifoo.scarl.core.ai.Tactic.Result
-import io.github.fiifoo.scarl.core.ai.{Behavior, Intention, Priority}
+import io.github.fiifoo.scarl.core.ai.{Behavior, Intention, Priority, Tactic}
 import io.github.fiifoo.scarl.core.entity.CreatureId
 
 import scala.util.Random
 
 case object GreetTactic extends Behavior {
 
-  val intentions: List[(Intention, Priority.Value)] = List((
-    CheckGreetIntention,
-    Priority.infinite
-  ))
+  val intentions: List[(Intention, Priority.Value)] = List()
 
   def behavior(s: State, actor: CreatureId, random: Random): Result = {
-    apply(s, actor, random) getOrElse(this, PassAction)
+    CheckGreetIntention(s, actor, random) getOrElse(this, PassAction)
+  }
+
+  override def apply(s: State, actor: CreatureId, random: Random): Option[(Tactic, Action)] = {
+    Some(behavior(s, actor, random))
   }
 }
