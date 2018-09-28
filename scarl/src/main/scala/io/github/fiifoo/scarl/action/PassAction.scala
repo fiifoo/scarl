680e577da8e1f49d36e95ab27ad4014ec3cc58d9
Taavi Lappalainen <taavi.lappalainen@gmail.com>
2018-09-28 19:01:31 +0300

    Use creature speed for pass action cost

--- scarl/src/main/scala/io/github/fiifoo/scarl/action/PassAction.scala
+++ scarl/src/main/scala/io/github/fiifoo/scarl/action/PassAction.scala
@@ -5,9 +5,12 @@ import io.github.fiifoo.scarl.core.action.Action
 import io.github.fiifoo.scarl.core.effect.Effect
 import io.github.fiifoo.scarl.core.entity.CreatureId
 import io.github.fiifoo.scarl.effect.TickEffect
+import io.github.fiifoo.scarl.rule.MoveRule
 
 case object PassAction extends Action {
   def apply(s: State, actor: CreatureId): List[Effect] = {
-    List(TickEffect(actor))
+    val cost = MoveRule.cost(s, actor)
+
+    List(TickEffect(actor, cost))
   }
 }
