d4363bc5d785ac19a76e9c461b91d7cdd9111dec
Taavi Lappalainen <taavi.lappalainen@gmail.com>
2019-05-02 22:24:48 +0300

    Fix infinite loop in model scan

--- app/models/admin/Models.scala
+++ app/models/admin/Models.scala
@@ -23,18 +23,18 @@ object Models {
         val subs = result._2
 
         subs map (sub => sub.id -> sub)
-      })).toMap,
+      })).toMap
     )
   }
 
   private def scanModel(source: ModelSource): (Model, List[SubModel]) = {
-    val results = if (source.polymorphic.nonEmpty) List() else {
-      getProperties(source.t) map scanProperty(source.t)
+    val results = if (source.polymorphic.nonEmpty) Nil else {
+      getProperties(source.t) map scanProperty(Nil)(source.t)
     }
 
     val properties = results map (_._1)
     val subs = results flatMap (_._2)
-    val polymorphic = source.polymorphic flatMap scanSubModel
+    val polymorphic = source.polymorphic flatMap scanSubModel(Nil)
 
     val model = Model(
       id = source.id,
@@ -47,73 +47,73 @@ object Models {
     (model, subs ::: polymorphic)
   }
 
-  private def scanSubModel(t: Type): List[SubModel] = {
+  private def scanSubModel(path: List[Type])(t: Type): List[SubModel] = {
     val id = SubModel.Id(t)
     val source = Sources.sub.get(id)
 
-    val results = if (source.exists(_.polymorphic.nonEmpty)) List() else {
-      getProperties(t) map scanProperty(t)
+    val results = if (source.exists(_.polymorphic.nonEmpty)) Nil else {
+      getProperties(t) map scanProperty(path)(t)
     }
 
     val properties = results map (_._1)
     val subs = results flatMap (_._2)
-    val polymorphic = source map (_.polymorphic flatMap scanSubModel) getOrElse List()
+    val polymorphic = source map (_.polymorphic flatMap scanSubModel(t :: path)) getOrElse Nil
 
     val model = SubModel(
       id = id,
       properties = properties,
-      polymorphic = source map (_.polymorphic map SubModel.Id.apply) getOrElse List(),
-      objectPolymorphism = source exists (_.objectPolymorphism),
+      polymorphic = source map (_.polymorphic map SubModel.Id.apply) getOrElse Nil,
+      objectPolymorphism = source exists (_.objectPolymorphism)
     )
 
     model :: subs ::: polymorphic
   }
 
-  private def scanProperty(t: Type)(s: Symbol): (Property, List[SubModel]) = {
+  private def scanProperty(path: List[Type])(t: Type)(s: Symbol): (Property, List[SubModel]) = {
     val name = getPropertyName(s)
-    val (fieldType, subs) = scanField(t, s.typeSignatureIn(t))
+    val (fieldType, subs) = scanField(t :: path, s.typeSignatureIn(t))
 
     val property = Property(name.toString, fieldType)
 
     (property, subs)
   }
 
-  private def scanField(parent: Type, t: Type, required: Boolean = true): (FieldType, List[SubModel]) = {
+  private def scanField(path: List[Type], t: Type, required: Boolean = true): (FieldType, List[SubModel]) = {
     t match {
-      case t if t <:< typeOf[Option[_]] => scanField(parent, t.typeArgs.head, required = false)
+      case t if t <:< typeOf[Option[_]] => scanField(t :: path, t.typeArgs.head, required = false)
 
       case t if t <:< typeOf[Map[_, _]] =>
-        val (keyFieldType, keySubs) = scanField(parent, t.typeArgs.head)
-        val (valueFieldType, valueSubs) = scanField(parent, t.typeArgs(1))
+        val (keyFieldType, keySubs) = scanField(t :: path, t.typeArgs.head)
+        val (valueFieldType, valueSubs) = scanField(t :: path, t.typeArgs(1))
 
         (MapField(keyFieldType, valueFieldType, required), keySubs ::: valueSubs)
 
       case t if t <:< typeOf[Iterable[_]] =>
-        val (valueFieldType, subs) = scanField(parent, t.typeArgs.head)
+        val (valueFieldType, subs) = scanField(t :: path, t.typeArgs.head)
 
         (ListField(valueFieldType, required), subs)
 
-      case t if t =:= typeOf[Int] => (IntegerField(required, getScalarFieldAlias(t)), List())
-      case t if t =:= typeOf[Long] => (IntegerField(required, getScalarFieldAlias(t)), List())
-      case t if t =:= typeOf[Double] => (DecimalField(required, getScalarFieldAlias(t)), List())
-      case t if t =:= typeOf[Float] => (DecimalField(required, getScalarFieldAlias(t)), List())
-      case t if t =:= typeOf[Boolean] => (BooleanField(required, getScalarFieldAlias(t)), List())
-      case t if t =:= typeOf[Char] => (CharField(required, getScalarFieldAlias(t)), List())
-      case t if t =:= typeOf[String] => (StringField(required, getScalarFieldAlias(t)), List())
+      case t if t =:= typeOf[Int] => (IntegerField(required, getScalarFieldAlias(t)), Nil)
+      case t if t =:= typeOf[Long] => (IntegerField(required, getScalarFieldAlias(t)), Nil)
+      case t if t =:= typeOf[Double] => (DecimalField(required, getScalarFieldAlias(t)), Nil)
+      case t if t =:= typeOf[Float] => (DecimalField(required, getScalarFieldAlias(t)), Nil)
+      case t if t =:= typeOf[Boolean] => (BooleanField(required, getScalarFieldAlias(t)), Nil)
+      case t if t =:= typeOf[Char] => (CharField(required, getScalarFieldAlias(t)), Nil)
+      case t if t =:= typeOf[String] => (StringField(required, getScalarFieldAlias(t)), Nil)
 
       case t if Sources.main.isDefinedAt(Model.RelationId.fromProperty(t)) =>
         val source = Sources.main(Model.RelationId.fromProperty(t))
 
-        (RelationField(source.id, required), List())
+        (RelationField(source.id, required), Nil)
 
       case t if Sources.polymorphicRelation.isDefinedAt(Model.RelationId.fromProperty(t)) =>
         val source = Sources.polymorphicRelation(Model.RelationId.fromProperty(t))
 
-        (PolymorphicRelationField(source.models, required), List())
+        (PolymorphicRelationField(source.models, required), Nil)
 
       case t if isOwnType(t) || t <:< typeOf[(_, _)] || t <:< typeOf[(_, _, _)] =>
         val id = SubModel.Id(t)
-        val subs = if (t =:= parent) List() else scanSubModel(t)
+        val subs = if (path exists (_ =:= t)) Nil else scanSubModel(t :: path)(t)
 
         (FormField(id, required), subs)
 
